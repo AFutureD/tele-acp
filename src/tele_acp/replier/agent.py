f6c791a5df8cca139515ada5536b638b8097d901
Huanan <donghuanan@inston.ltd>
2026-03-19 15:13:41 +0800

    .

--- src/tele_acp/replier/agent.py
+++ src/tele_acp/replier/agent.py
@@ -58,6 +58,7 @@ class ChatReplier(AgentThread, ChatMessageReplyable):
         async for delta in stream:
             if (stop_reason := delta.stop_reason) and stop_reason != "cancelled":
                 msg = convert_acp_message_to_chat_message(message.channel_id, message.chat_id, delta)
-                msg.receiver = self.settings.forward_to
+                if (forward_to := self.settings.forward_to) and forward_to != "":
+                    msg.receiver = forward_to
                 await chat.send_message(msg)
         self.logger.info("Message sent for peer: %s", message.channel_id)
